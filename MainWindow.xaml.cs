f2f6bd1f0fd1d7574f770fc3062c3f536ff7db7b
callisto1337 <hazardous333@gmail.com>
2021-10-17 01:27:11 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoList
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstTodoList.Items.Contains(txtName.Text))
+            {
+                MessageBox.Show("Такая заметка уже существует!");
+            }
+            else if (!string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                lstTodoList.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstTodoList.SelectedIndex >= 0)
+            {
+                lstTodoList.Items.Remove(lstTodoList.SelectedItem);
+            }
+        }
+    }
+}
